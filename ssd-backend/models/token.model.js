c485eb3d91e7388fb4553313a9b49ebc4c8d5063
Ravindu Deshan <49786108+RavinduDeshan@users.noreply.github.com>
2021-10-03 17:18:49 +0530

    token js model added

---
+++ ssd-backend/models/token.model.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const tokenScehema = new schema({
+    token: {
+        type: String,
+        // required: true
+    },
+})
+module.exports = Token = mongoose.model('token', tokenScehema)
