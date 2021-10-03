f719b6968c561c3beb534b5e6a9832b6c080ff32
N A Yapa <it18002102@my.sliit.lk>
2021-10-03 18:13:16 +0530

    Initial frontend added

---
+++ ssd-frontend/src/App.js
@@ -0,0 +1,30 @@
+//NISHIKI
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+// import SignIn from "./components/SignIn";
+import Form from "./components/Form";
+import HomePage from "./components/HomePage";
+import UploadMainFile from "./components/UploadMainFile";
+import ImportFile from "./components/ImportFile";
+import UploadFile from "./components/UploadFile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <main>
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/SignIn" component={Form} />
+        <Route path="/Home" component={HomePage} />
+        <Route path="/Upload" component={UploadMainFile} />
+        <Route path="/Import" component={ImportFile} />
+        <Route path="/uploadRedirectPage" component={UploadFile} />
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+//------------------------------------------------------------------
